d00454f40535bd95c73ef113c043c1068c109f52
Bl4cKc34sEr <63438820+Bl4cKc34sEr@users.noreply.github.com>
2020-10-10 11:55:56 +0530

    Added Morse.py
    Morse code to text converter

---
+++ Morse.py
@@ -0,0 +1,50 @@
+MORSE_CODE = { 'A':'.-', 'B':'-...',
+   'C':'-.-.', 'D':'-..', 'E':'.',
+   'F':'..-.', 'G':'--.', 'H':'....',
+   'I':'..', 'J':'.---', 'K':'-.-',
+   'L':'.-..', 'M':'--', 'N':'-.',
+   'O':'---', 'P':'.--.', 'Q':'--.-',
+   'R':'.-.', 'S':'...', 'T':'-',
+   'U':'..-', 'V':'...-', 'W':'.--',
+   'X':'-..-', 'Y':'-.--', 'Z':'--..',
+   '1':'.----', '2':'..---', '3':'...--',
+   '4':'....-', '5':'.....', '6':'-....',
+   '7':'--...', '8':'---..', '9':'----.',
+   '0':'-----', ', ':'--..--', '.':'.-.-.-',
+   '?':'..--..', '/':'-..-.', '-':'-....-',
+   '(':'-.--.', ')':'-.--.-' ,'_':'..--.-'
+}
+def encryption(message):
+   cipher_text = ''
+   for mymessage in message:
+      if mymessage != ' ':
+         cipher_text += MORSE_CODE[mymessage] + ' '
+      else:
+         cipher_text += ' '
+      return cipher_text
+
+def decryption(message):
+   message += ' '
+   decipher = ''
+   mycitext = ''
+   for mymessage in message:
+      if (mymessage != ' '):
+         i = 0
+         mycitext += mymessage
+      else:
+         i += 1
+         if i == 2 :
+            decipher += ' '
+         else:
+            decipher += list(MORSE_CODE.keys())[list(MORSE_CODE
+            .values()).index(mycitext)]
+            mycitext = ''
+   return decipher
+def main():
+   
+   my_message = input("Enter Morse Code:")
+   output = decryption(my_message)
+   print (output)
+
+if __name__ == '__main__':
+   main()
