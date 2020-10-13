a41d859e8091aa193481360cc0352d9646cabbc3
Shailesh12-svg <69112925+Shailesh12-svg@users.noreply.github.com>
2020-10-13 12:49:11 +0530

    Python program for Bmi calculator

---
+++ Question.py
@@ -0,0 +1,7 @@
+Name=str(input("Enter your Name"))
+Height=float(input("The height of the Person in (m)  is :")) #We are Giving 1st input as Height .
+Weight=float(input("The weight of the Person in (kg) is:"))#We are Giving 2nd input as Weight
+Body_Mass_Index=(Weight/(Height**2))#Formula for BMI
+print("The Name of the Person:",Name)
+print("The BMI of the Person:",Body_Mass_Index)#To print the Body_Mass_Index of person.
+
